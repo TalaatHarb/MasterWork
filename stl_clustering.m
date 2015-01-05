29256c3fb3c076f5b91e048a0123f2d990e26b75
Mohamed Talaat Harb <mohamed.talaatharb@eng.asu.edu.eg>
2015-01-05 10:50:08 +0200

    Adding the first file that contains only loading STL functionality

---
+++ stl_clustering.m
@@ -0,0 +1,33 @@
+%% Clearing Old Data
+clear
+clc
+
+%% Opening the file
+filename = 'ChessSet.stl';
+[v, f, n, c, stltitle] = stlread(filename);
+[v, f]=patchslim(v, f);
+
+% Number of vertices
+num_points = size(v,1);
+
+% Number of faces
+num_faces = size(f,1);
+
+% Display file parameters
+disp(['File Name:          ' filename]);
+disp(['Number of vertices: ' num2str(num_points)]);
+disp(['Number of faces:    ' num2str(num_faces)]);
+
+return
+%% Clustering
+do_cluster_141223;
+
+%% Plotting the clusters
+cmap = hsv(k);
+for cluster = 1:k
+    plot3(m(cluster,1),m(cluster,2),m(cluster,3),'o','Color',cmap(cluster,:));
+    hold on;
+    plot3(v(id(1:count(cluster),cluster),1),v(id(1:count(cluster),cluster),2),v(id(1:count(cluster),cluster),3),'+','Color',cmap(cluster,:));
+end
+patch('Faces',f,'Vertices',v,'FaceVertexCData',c);
+hold off;
