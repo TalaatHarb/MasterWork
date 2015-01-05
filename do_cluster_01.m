21907952ea81f92d20c6523e9d01ae8aa389adbb
Mohamed Talaat Harb <mohamed.talaatharb@eng.asu.edu.eg>
2015-01-05 17:16:29 +0200

    Adding connectivity to the clustering

--- do_cluster_01.m
+++ do_cluster_01.m
@@ -9,13 +9,22 @@ num_points = size(v,1);
 num_faces = size(f,1);
 
 % Array to store which points are in a cluster and which are not
-clusterID = zeros(num_points, 1);
-currentCluster = 1;
-tolerance = 1e-6;
+PlaneID = zeros(num_points, 1);
+currentPlane = 1;
+tolerance = 9e-7;
 distanceMeasure = zeros(num_points, num_points);
+connectivity = zeros(num_points, num_points);
+for faceCounter = 1:num_faces
+    connectivity(f(faceCounter,1),f(faceCounter,2)) = 1;
+    connectivity(f(faceCounter,2),f(faceCounter,1)) = 1;
+    connectivity(f(faceCounter,2),f(faceCounter,3)) = 1;
+    connectivity(f(faceCounter,3),f(faceCounter,2)) = 1;
+    connectivity(f(faceCounter,3),f(faceCounter,1)) = 1;
+    connectivity(f(faceCounter,1),f(faceCounter,3)) = 1;
+end
 for vertexCounter = 1:num_points
     % Ignoring already taken points
-    if clusterID(vertexCounter,1) ~= 0
+    if PlaneID(vertexCounter,1) ~= 0
         continue;
     else
         currentVertex = v(vertexCounter, :);
@@ -46,31 +55,40 @@ for vertexCounter = 1:num_points
             end
         end
         [~,I] = sort(distances(:,1));
-        indexes_of_closest_2 = I(1:2,1);
-        closestTwo = neighborhood(indexes_of_closest_2,:);
+        closestTwo = neighborhood(I(1:2,1),:);
         % Calculating the vertex plane
         normal = cross(closestTwo(1,:)-currentVertex, currentVertex-closestTwo(2,:));
         D = -dot(normal,currentVertex);
         for vertexCounter2 = 1:num_points
             % Ignoring already taken points
-            if clusterID(vertexCounter2,1) ~= 0
+            if PlaneID(vertexCounter2,1) ~= 0
                 continue;
             else
                 % Add all verteces in the plane to the current cluster
                 proximity_to_plane = abs((dot(normal, v(vertexCounter2,:))+ D)/norm(v(vertexCounter2,:)));
                 if proximity_to_plane <= tolerance
-                    clusterID(vertexCounter2,1) = currentCluster;
+                    PlaneID(vertexCounter2,1) = currentPlane;
                 end
             end
         end
-        currentCluster = currentCluster + 1;
+        currentPlane = currentPlane + 1;
     end
 end
-% Caculating the number of cross sections
-k = max(clusterID(:));
-
-result = {num_points;num_faces;k;v;f;clusterID};
-for cluster = 1:k
-    result{6+cluster} = v(clusterID == cluster,:);
+% Caculating the number of planes
+p = max(PlaneID(:));
+k = p;
+ClusterID = PlaneID;
+result = {num_points;...
+    num_faces;...
+    p;...
+    k;...
+    v;...
+    f;...
+    connectivity;...
+    PlaneID;...
+    ClusterID...
+    };
+for plane = 1:p
+    result{9+plane} = v(PlaneID == plane,:);
 end
 end
--- stl_clustering.m
+++ stl_clustering.m
@@ -1,20 +1,23 @@
 %% Clearing Old Data
-clear all
-close all
-clc
+clear all;
+clf;
+clc;
 tic;
 %% Opening the file
-filename = 'multi.stl';
+filename = 'test.stl';
 
 %% Clustering
 result = do_cluster_01(filename);
 
 num_points = result{1};
 num_faces = result{2};
+p = result{3};
 k = result{3};
-v = result{4};
-f = result{5};
-clusterID = result{6};
+v = result{5};
+f = result{6};
+connectivity = result{7};
+PlaneID = result{8};
+ClusterID = result{9};
 
 %% Plotting the clusters
 % Display file parameters
@@ -27,19 +30,32 @@ disp(['Number of cross sections:          ' num2str(k)]);
 colors = jet(k);
 
 for cluster = 1:k
-    current_cluster = result{6+cluster};
+    
+    current_cluster = result{9+cluster};
+    
+    % Displaying some information about the cluster
+    fprintf('\n');
+    disp(['Cluster ' num2str(cluster)]);
+    disp(['Cluster Center:                    ' num2str(mean(current_cluster))]);
+    disp(['Cluster Radius:                    ' num2str(norm(std(current_cluster)))]);
     X = current_cluster(:,1);
     Y = current_cluster(:,2);
     Z = current_cluster(:,3);
     S = 16;
     C = repmat(colors(cluster,:),numel(X),1);
+    
     scatter3(X,Y,Z,S,C);
     hold on;
+    
     clearvars X Y Z S C
 end
-clearvars colors
+
+clearvars colors cluster current_cluster
+
 patch('Faces',f,'Vertices',v);
 alpha(0.5);
 hold off;
+
+fprintf('\n');
 
 toc;
